313e0bcfebe80384fdd5e8e6a00acac553429689
Alex Ahijado <alejandro.ahijado@gmail.com>
2023-10-04 11:01:05 +0200

    commited to merge

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,17 +1,20 @@
 import React from "react";
-import ExpenseDate from './ExpenseDate';
-import './ExpenseItem.css'
-import Card from '../UI/Card';
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
 function ExpenseItem(props) {
-
+  const clickHandler = () => {
+    
+  }
 
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
-      <div className='expense-item__description'>
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
